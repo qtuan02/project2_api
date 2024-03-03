6865a9551129c91c6f6ce082b1b5ad9be172c50d
qtuan02 <quoctuan200702@gmail.com>
2024-03-03 11:26:11 +0700

    main project cafe

---
+++ src/main/java/com/project4/Project4ApiApplication.java
@@ -0,0 +1,13 @@
+package com.project4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project4ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project4ApiApplication.class, args);
+	}
+
+}
